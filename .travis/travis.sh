14649c1b8051cf6584fd2c1174470593d5cb4c20
Henning Kayser <henningkayser@picknik.ai>
2019-01-17 20:01:51 -0700

    Add rtr_moveit_deb_files dependencies to Travis

--- .travis/travis.sh
+++ .travis/travis.sh
@@ -17,6 +17,7 @@ export HIT_ENDOFSCRIPT=false
 export REPOSITORY_NAME=${PWD##*/}
 cd ..
 export CATKIN_WS=${PWD}/ws_moveit
+export RAPIDPLAN_DEBS=$CI_SOURCE_PATH/rtr_moveit_deb_files
 echo "---"
 echo "Testing branch '$TRAVIS_BRANCH' of '$REPOSITORY_NAME' on ROS '$ROS_DISTRO'"
 
@@ -36,6 +37,9 @@ if [ "$IN_DOCKER" -eq 0 ]; then
   # Create workspace in root Travis environment where the SSH keys are easily accessible
   travis_run mkdir -p $CATKIN_WS/src
   travis_run cd $CATKIN_WS/src
+
+  # clone rapidplan deb files
+  travis_run git clone https://github.com/PickNikRobotics/rtr_moveit_deb_files.git $RAPIDPLAN_DEBS
 
   # Move repo we are testing into catkin ws
   sudo mv $CI_SOURCE_PATH $CATKIN_WS/src
@@ -151,6 +155,15 @@ export PATH=/usr/lib/ccache:$PATH
 
 # Setup rosdep - note: "rosdep init" is already setup in base ROS Docker image
 travis_run rosdep update
+
+# Install rapidplan dependencies
+travis_run apt-get -qq install ros-kinetic-pcl-ros
+travis_run apt-get -qq install ros-kinetic-trac-ik-lib
+travis_run apt-get -qq install libstdc++6
+travis_run apt-get -qq install gdebi
+travis_run gdebi --n $RAPIDPLAN_DEBS/rapidplan/rapid-plan-16.04.deb
+travis_run dpkg -i $RAPIDPLAN_DEBS/rtr_toolkit/ros-kinetic-*.deb
+travis_run apt-get -yf install
 
 # TODO: restore run before script
 set -x
